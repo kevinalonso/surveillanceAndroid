d2d8949c0b248b71741236c37ea214d25222d2b5
kevin <k.alonso@iia-laval.fr>
2016-01-19 16:09:07 +0100

    Reload state origin

--- app/src/main/java/iia/com/surveillanceproject/MainActivity.java
+++ app/src/main/java/iia/com/surveillanceproject/MainActivity.java
@@ -9,12 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
-        //test2
-        //shghshjfghg/
-        //yolo
-        //bonjour
-        // Coucou pd
+
 
     }
 }
